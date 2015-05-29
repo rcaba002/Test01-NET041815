a36640685e16601a642dbc798129c7086b99daf0
Andrew Benz <andorbal@gmail.com>
2015-05-28 22:15:08 -0500

    Initial commit

---
+++ Test01/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test01
+{
+    public class TestClass
+    {
+        [Fact]
+        public void Reduce_RemovesElementsLessThanThree()
+        {
+            TestObject testObject = new TestObject();
+            List<int> input = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> result = testObject.Reduce(input);
+            Assert.Collection(result, x => Assert.Equal(4, x), x => Assert.Equal(5, x));
+        }
+
+        [Fact]
+        public void Reduce_RemovesElementsMoreThanTen()
+        {
+            TestObject testObject = new TestObject();
+            List<int> input = new List<int> { 8, 9, 10, 11, 12 };
+            List<int> result = testObject.Reduce(input);
+            Assert.Collection(result, x => Assert.Equal(8, x), x => Assert.Equal(9, x));
+        }
+    }
+}
---
+++ Test01/TestObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test01
+{
+    class TestObject
+    {
+        internal List<int> Reduce(List<int> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
