2760236a8b5ca05b0014603db003b72818705f77
kmrands <kmrands@gmail.com>
2015-05-30 11:33:04 -0500

    All tests work

--- Test01/TestObject.cs
+++ Test01/TestObject.cs
@@ -7,9 +7,21 @@ namespace Test01
 {
     class TestObject
     {
+        //removes elements less than four
+        //removes elements more than ten
+        //removes duplicates
+        //returns first five items
+
         internal List<int> Reduce(List<int> input)
         {
-            throw new NotImplementedException();
+            List<int> resultsOne = new List<int>();
+
+            input.Sort();
+
+            resultsOne = input.Where(x => x >= 4).Where(x => x < 10)
+                .GroupBy(x => x).Select(y => y.First()).Take(5).ToList();
+
+            return resultsOne;
         }
     }
 }
