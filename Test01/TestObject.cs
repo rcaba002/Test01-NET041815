93c7b35754a388c32b46fcc0510566cea189a876
Johnny Sysenkham <jsysenkham@comcast.net>
2015-05-30 10:59:02 -0500

    removing dupes needs work saving as other 3 work.

--- Test01/TestObject.cs
+++ Test01/TestObject.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace Test01
 {
     class TestObject
     {
-        internal List<int> Reduce(List<int> input)
+        internal List<int> ReduceLessThanFour(List<int> input)
         {
-            throw new NotImplementedException();
+            return input.Where(x => x >= 4).Select(x => x).ToList();
+        }
+
+        internal List<int> RemovesElementsMoreThanTen(List<int> input)
+        {
+            return input.Where(x => x < 10).Select(x => x).ToList();
+        }
+
+        internal List<int> RemovesDuplicateElements(List<int> input)
+        {
+            return input.GroupBy(x => x).Where(x => x.Count() < 2).Select(x => x.Key).ToList();
+        }
+
+        internal List<int> ReturnsFirstFiveItems(List<int> input)
+        {
+            return input.GetRange(0, 5).ToList();
         }
     }
 }
