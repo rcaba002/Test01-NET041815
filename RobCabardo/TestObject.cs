f9cedf6509a5c20d3b03c0694ce5b7e5efc6cb04
Rob C <robcabardo@gmail.com>
2015-05-30 13:10:26 -0500

    initial commit

---
+++ RobCabardo/TestObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Test01
+{
+    class TestObject
+    {
+        internal List<int> Reduce(List<int> input)
+        {
+            List<int> results = input.Where(x => x >= 4 && x < 10).Distinct().Take(5).ToList();
+
+            return results;
+        }
+    }
+}
